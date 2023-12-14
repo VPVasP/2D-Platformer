5048130cb53180735fb09e63aa569007c44e3fb3
VP <102414151+VPVasP@users.noreply.github.com>
2023-12-14 14:36:03 +0200

    Raycast GroundCheck for Jumping

--- 2DPlatformer/Assets/Scripts/Controller.cs
+++ 2DPlatformer/Assets/Scripts/Controller.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public LayerMask groundMask;
     Animator thisAnimator;
     Rigidbody2D rb;
     float inpHor;
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce;
     SpriteRenderer spriteRenderer;
+    [SerializeField] bool isGrounded = false;
+    [SerializeField] float groundDistance = 0.6f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +25,9 @@ public class Controller : MonoBehaviour
     void Update()
     {
         inpHor = Input.GetAxis("Horizontal");
-
+        isGrounded = isGroundedBool();
         //transform.position += Vector3.right * inpHor * speed * Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) &&isGroundedBool())
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
         }
@@ -36,12 +39,20 @@ public class Controller : MonoBehaviour
         {
             spriteRenderer.flipX = false;
 
-            thisAnimator.SetFloat("Speed", Mathf.Abs(inpHor));
+        
         }
+        thisAnimator.SetFloat("Speed", Mathf.Abs(inpHor));
+        thisAnimator.SetBool("isGrounded", isGrounded);
     }
     private void FixedUpdate()
     {
 
         rb.velocity = new Vector2(inpHor *speed, rb.velocity.y);
+    }
+    bool isGroundedBool()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down,groundDistance,groundMask);
+        if (hit.collider != null) { return true; }
+        else return false;
     }
 }
